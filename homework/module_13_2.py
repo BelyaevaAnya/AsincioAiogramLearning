5f9e2a3adb83f241b07f130d72598979ca6b0a2a
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-10-03 17:35:09 +0300

    Update module_13_2.py

--- homework/module_13_2.py
+++ homework/module_13_2.py
@@ -30,9 +30,6 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    asyncio.run(main())
-=======
     # Запускаем основной цикл через asyncio
     asyncio.run(main())
 
